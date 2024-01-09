7b8a2c18cdce789da4d5826712aef157610f179b
TrangDC <dinhcongtrang99@gmail.com>
2024-01-09 15:37:45 +0700

    copy code từ dự án spring mvc thymeleaf cũ. Hiện đang lỗi ảnh và chức năng phân trang

---
+++ src/main/java/com/example/demo2productmanagespringboot/formatter/CategoryFormatter.java
@@ -0,0 +1,33 @@
+package com.example.demo2productmanagespringboot.formatter;
+
+import com.example.demo2productmanagespringboot.model.Category;
+import com.example.demo2productmanagespringboot.service.ICategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.Formatter;
+import org.springframework.stereotype.Component;
+
+import java.text.ParseException;
+import java.util.Locale;
+import java.util.Optional;
+
+@Component
+public class CategoryFormatter implements Formatter<Category> {
+
+    private final ICategoryService categoryService;
+
+    @Autowired
+    public CategoryFormatter(ICategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @Override
+    public Category parse(String text, Locale locale) throws ParseException {
+        Optional<Category> classroomOptional = categoryService.findById(Long.parseLong(text));
+        return classroomOptional.orElse(null);
+    }
+
+    @Override
+    public String print(Category object, Locale locale) {
+        return "[" + object.getId() + ", " + object.getName() + "]";
+    }
+}
